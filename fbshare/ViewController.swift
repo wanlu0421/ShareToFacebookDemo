082680c39e7a35bdd0180ca31e1dcc15de35357b
Wanlu Wei <wanlu0421@gmail.com>
2016-09-18 19:28:57 -0400

    Initial Commit

---
+++ fbshare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fbshare
+//
+//  Created by wanlu wei on 2016-09-18.
+//  Copyright © 2016 wanlu wei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
