63c647b29a70a1e119e74f62ab9ce5e9fc368133
Wanlu Wei <wanlu0421@gmail.com>
2016-09-18 23:03:23 -0400

    creadted layout

---
+++ fbshare/SharedItem.swift
@@ -0,0 +1,18 @@
+//
+//  SharedItem.swift
+//  fbshare
+//
+//  Created by wanlu wei on 2016-09-18.
+//  Copyright © 2016 wanlu wei. All rights reserved.
+//
+
+import Foundation
+
+class SharedItem{
+    var name:String
+    
+    init(name:String){
+        self.name = name
+    }
+}
+
---
+++ fbshare/SharedTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  SharedTableViewCell.swift
+//  fbshare
+//
+//  Created by wanlu wei on 2016-09-18.
+//  Copyright © 2016 wanlu wei. All rights reserved.
+//
+
+import UIKit
+
+class SharedTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var LableTitle: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
